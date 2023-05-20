dc30334f7839e4669901f36f0cf6c1dc2abe5f3e
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-20 19:02:00 +0500

    update files

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,10 +1,10 @@
 import { CreateSlice, createSlice } from "@reduxjs/toolkit";
 
-const initialState {
-  cartItems = [],
-  amount = 0,
-  total = 0,
-  isLoading = true, 
+const initialState = {
+  cartItems: [],
+  amount: 0,
+  total: 0,
+  isLoading: true, 
 };
 
 const cartSlice = createSlice({
